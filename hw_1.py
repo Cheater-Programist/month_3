d8cd1395bf109c74c3053d2b48ef8891078b6e47
Cheater-Programist <muhammadaminibaydillayev@gmail.com>
2024-01-29 18:41:30 +0600

    first commit

---
+++ hw_1.py
@@ -0,0 +1,34 @@
+from aiogram import Bot, Dispatcher, types, executor
+import random
+
+bot = Bot(token='6789612758:AAGugs4UyhKEaBwLt32gTenbPB_5lbo6Iv8')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands='start')
+async def start(message:types.Message):
+    await message.answer(' Я загадал число от 1 до 3 угадайте')
+
+a = random.randint(1, 3)
+
+@dp.message_handler(text = ['1'])
+async def start(message:types.Message):
+    if 1 == a:
+        await message.answer_photo('https://media.makeameme.org/created/you-win-nothing-b744e1771f.jpg')
+    else:
+        await message.answer_photo('https://media.makeameme.org/created/sorry-you-lose.jpg')
+
+@dp.message_handler(text = ['2'])
+async def start(message:types.Message):
+    if 2 == a:
+        await message.answer_photo('https://media.makeameme.org/created/you-win-nothing-b744e1771f.jpg')
+    else:
+        await message.answer_photo('https://media.makeameme.org/created/sorry-you-lose.jpg')
+
+@dp.message_handler(text = ['3'])
+async def start(message:types.Message):
+    if 3 == a:
+        await message.answer_photo('https://media.makeameme.org/created/you-win-nothing-b744e1771f.jpg')
+    else:
+        await message.answer_photo('https://media.makeameme.org/created/sorry-you-lose.jpg')
+
+executor.start_polling(dp)
---
+++ start_bot.py
@@ -0,0 +1,38 @@
+from aiogram import Bot, Dispatcher, types, executor
+
+bot = Bot(token='6843613788:AAGMP6KbFd2oh52snaAUD49fvHXr8Q-x2zA')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands='start')
+async def start(massage:types.Message):
+    await massage.answer("Hello World!, and Geeks")
+
+@dp.message_handler(commands='help')
+async def help(message:types.Message):
+    await message.answer("How can I help you?")
+
+@dp.message_handler(text='Geeks')
+async def geeks(message:types.Message):
+    await message.answer("Geeks")
+
+@dp.message_handler(commands='test')
+async def test(message:types.Message):
+    print(message)
+    # await message.answer(f"Hello {message.from_user.full_name}")
+    # await message.answer(f'Your username is @{message.from_user.username}')
+    # await message.reply("It's repling a text")
+    # await message.answer_location(40.5193216724554, 72.8030109959693)
+    # await message.answer_photo('https://data.kaktus.media/image/big/2023-07-21_18-00-13_831965.jpg')
+    # await message.answer_contact('+996552878777', "Mukhammadamin", "Ibaydillayev")
+    # await message.answer_dice(emoji="🎯")
+    with open('voice.m4a', 'rb') as my_voice:
+        await message.answer_voice(my_voice)
+    with open('voice.m4a', 'rb') as my_voice:
+        await message.answer_audio(my_voice)
+
+
+@dp.message_handler()
+async def not_found(message:types.Message):
+    await message.reply("I didn't care, chose /help")
+
+executor.start_polling(dp)
